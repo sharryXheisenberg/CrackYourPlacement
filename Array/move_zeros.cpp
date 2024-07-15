f717654e5e3f87dd4d73c83e2b615a2d83f2e77d
sharryXheisenberg <132823681+sharryXheisenberg@users.noreply.github.com>
2024-07-15 21:57:26 +0530

    Day 1

---
+++ Array/move_zeros.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<bits/stdc++.h>
+#include<algorithm>
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+        }        
+    }
+};
